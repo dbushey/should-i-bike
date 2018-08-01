74a3408f0ce80550a8448c408c808937ecd47ba0
dbushey <polendeuva@gmail.com>
2018-08-01 17:13:19 -0400

    trying to change time to user input time

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,25 +1,25 @@
 module Api
 
-  def googleMaps(origin, destination, time)
-    url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&departure_time=#{time}&mode=bicycling&key=AIzaSyAQCsSJwynJ67ODIv0ZgGwCffJSChdvB9o"
+  def googleDirections(origin, destination, departure_time)
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&departure_time=#{departure_time}&mode=bicycling&key=AIzaSyAQCsSJwynJ67ODIv0ZgGwCffJSChdvB9o"
 
     # localhost:3000/direction?origin=1176+Greene+Avenue+Brooklyn+NY&destination=28W+23rd+St+New%20York+NY&departure_time=now
 
     response = HTTParty.get(url)
     my_response = {}
-    my_response["duration"] = response["routes"][0]["legs"][0] ["duration"]["text"]
+    # my_response["duration"] = response["routes"][0]["legs"][0] ["duration"]["text"]
     my_response["start_location"] = response["routes"][0]["legs"][0]["start_location"]
     my_response["end_location"] = response["routes"][0]["legs"][0]["end_location"]
 
     my_response
   end
 
-  def darksky(lat, lng, time)
-    url = "https://api.darksky.net/forecast/668446174c1c15bb92c097fc64d9b4ce/#{lat},#{lng}?exclude=currently,minutely,daily,flags"
+  def darksky(lat, lng, departure_time)
+    url = "https://api.darksky.net/forecast/668446174c1c15bb92c097fc64d9b4ce/#{lat},#{lng},#{departure_time}?exclude=currently,minutely,daily,flags"
 
     response = HTTParty.get(url)
     my_response = {}
     response.parsed_response
   end
-  
+
 end
